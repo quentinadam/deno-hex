041043eb44862f8eb1e8b83cf822552dad50ac7d
Quentin Adam <quentinadam@gmail.com>
2025-09-13 10:54:16 +0700

    Upgrade Typescript to version 5.9.2. Bump version to 0.1.4.

--- src/hex.ts
+++ src/hex.ts
@@ -6,7 +6,7 @@ import assert from '@quentinadam/assert';
  * @param string The hex encoded string.
  * @returns The decoded buffer.
  */
-export function decode(hex: string): Uint8Array {
+export function decode(hex: string): Uint8Array<ArrayBuffer> {
   assert(/^[0-9a-fA-F]*$/.test(hex), 'Hex string must only contain hex characters');
   assert(hex.length % 2 === 0, 'Hex string length must be even');
   const buffer = new Uint8Array(hex.length / 2);
@@ -22,6 +22,6 @@ export function decode(hex: string): Uint8Array {
  * @param buffer The buffer to encode.
  * @returns The hex encoded string.
  */
-export function encode(buffer: Uint8Array): string {
+export function encode(buffer: Uint8Array<ArrayBuffer>): string {
   return Array.from(buffer).map((byte) => byte.toString(16).padStart(2, '0')).join('');
 }
